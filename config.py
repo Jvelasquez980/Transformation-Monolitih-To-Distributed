aa620b3047d637807f151bcb4ec71102546154e0
Jvelasquez980 <jeronimo.velasquez.escobar@gmail.com>
2025-05-11 16:59:59 -0500

    Update database configuration and remove unused MySQL service from docker-compose

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@18.235.143.239:3306/bookstore'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
